397ada916c05ddfaead68d35b843a92ca5c76d44
Endy Hardy <endy0611@gmail.com>
2016-10-04 20:59:45 +0800

    Event archive page

--- wp-content/themes/englishdept/archive-event.php
+++ wp-content/themes/englishdept/archive-event.php
@@ -3,31 +3,46 @@
 	<section>
 		
 		<div class="row">
-			<div class="col-lg-8">
+			<div class="col-lg-9">
 				<?php if(have_posts()): ?>
 					<h1>
 						<?php if (isset($_GET['ondate'])): ?>
-							What's on at <?php echo date('M j, Y', strtotime($_GET['ondate'] . ' 00:00:00')) ?>
+							What's on at <?php echo date('M j, Y', strtotime($_GET['ondate'])) ?>
+						<?php else: ?>
+							Upcoming Events
 						<?php endif ?>
 					</h1>
-					<?php while(have_posts()): the_post() ?>
-						<div class="post-list-item">
-							<h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
-							<div class="ellipsis-multiline">
-								<?php the_content(); ?>
+					<div class="post-list">
+						<?php while(have_posts()): the_post() ?>
+							<div class="post-list-item">
+								<h3>
+									<a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+								</h3>
+								<div class="text-muted">
+									<?php eo_the_start('G:i a') ?>
+									<?php if (eo_get_the_start('H:i') != eo_get_the_end('H:i')) echo 'to ' . eo_get_the_end('G:i a') ?>
+								</div>
+								<div class="ellipsis">
+									<?php the_content(); ?>
+								</div>
+								<div style="margin-top:5px;">
+									<?php foreach(get_the_terms(get_the_ID(), 'event-tag') as $tag): ?>
+										<a href="<?php echo get_tag_link($tag) ?>" class="label label-primary"><?php echo ucfirst($tag->name) ?></a>
+									<?php endforeach; ?>
+								</div>
 							</div>
-							<div style="margin-top:5px;">
-								<?php foreach(get_the_tags() as $tag): ?>
-									<a href="<?php echo get_tag_link($tag) ?>" class="label label-primary"><?php echo $tag->name ?></a>
-								<?php endforeach; ?>
-							</div>
-						</div>
 
-					<?php endwhile ?>
+						<?php endwhile ?>
+					</div>
 				<?php else: ?>
+					<h1>Oops, there are no upcoming events yet.</h1>
 
-					
 				<?php endif ?>
+			</div>
+			<div class="col-lg-3">
+				<div class="sidebar sidebar-single">
+					<?php dynamic_sidebar('englishdept-aside-sidebar') ?>
+				</div>
 			</div>
 		</div>
 	</section>
